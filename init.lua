be0183f5f79f102d54d68053030de0ced402a53f
Wouter Heyvaert <wouter.heyvaert1@hotmail.com>
2023-09-12 21:49:48 +0200

    fix: remove LSPs and allow mouse interaction

--- init.lua
+++ init.lua
@@ -3,7 +3,6 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 vim.g.mapleader = " " -- Set leader key
-vim.opt.mouse = "" -- Disable mouse interaction
 vim.opt.spell = true -- Enable spell checking
 vim.opt.spelllang = "en" -- Set default spelling language
 
